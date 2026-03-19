ac47cf49326155bd1859c2d5a70413671ee6b3c5
Aryan Jain <jainaryan1818@gmail.com>
2026-03-19 15:11:45 +0530

    added some nodemailer functionalties

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -33,4 +33,35 @@ async function userRegisterController(req, res) {
     }
 }
 
-module.exports = { userRegisterController };
+// User login Controller
+async function userLoginController(req, res) {
+    try {
+        const { email, password } = req.body;
+
+        const user = await userModel.findOne({ email: email }).select("+password");
+        if (!user) {
+            return res.status(404).json({ message: "User not found", status: "failed" });
+        }
+
+        const isPasswordValid = await user.comparePassword(password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: "Invalid password", status: "failed" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" });
+        res.cookie("token", token);
+        return res.status(200).json({
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            token: token,
+        });
+    } catch (error) {
+        console.error("Login error:", error.message);
+        return res.status(500).json({ message: error.message || "Internal server error", status: "failed" });
+    }
+}
+
+module.exports = { userRegisterController, userLoginController };
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -5,6 +5,9 @@ const authController = require("../controllers/auth.controller");
 // POST /api/auth/register
 router.post("/register", authController.userRegisterController);
 
+//POST /api/auth/login
+router.post("/login", authController.userLoginController);  
+
 
 
 module.exports = router;    
